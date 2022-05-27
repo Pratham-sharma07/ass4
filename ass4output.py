431da7e2f9355ff25d63f4bb31fc89492a23bf11
Pratham-sharma07 <102985188+Pratham-sharma07@users.noreply.github.com>
2022-05-28 00:30:29 +0700

    Add files via upload

---
+++ ass4output.py
@@ -0,0 +1,46 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================================================================== RESTART: C:/Users/acer/Desktop/assignment4.py ====================================================================
+Grading system of a school!
+Enter your marks obtained( out of 100): 99
+Your Grade is: A
+
+Enter any Year:2020
+2020 is a Leap Year
+
+It's a one to one digit multiplication Game
+Question- 1 : 7 * 9
+Enter your result:63
+Right!
+Question- 2 : 7 * 9
+Enter your result:81
+Wrong! The correct answer is: 63
+Question- 3 : 2 * 2
+Enter your result:4
+Right!
+Question- 4 : 7 * 4
+Enter your result:67
+Wrong! The correct answer is: 28
+Question- 5 : 3 * 2
+Enter your result:48
+Wrong! The correct answer is: 6
+Question- 6 : 8 * 2
+Enter your result:16
+Right!
+Question- 7 : 2 * 5
+Enter your result:22
+Wrong! The correct answer is: 10
+Question- 8 : 8 * 8
+Enter your result:64
+Right!
+Question- 9 : 6 * 9
+Enter your result:54
+Right!
+Question- 10 : 7 * 9
+Enter your result:98
+Wrong! The correct answer is: 63
+
+
+There are total 177  candies in the bowl.
+
